1dbdecdb02e249804832007f4b8be02f31984afd
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-01-01 18:58:18 -0300

    * removed old hook for frontend view actions.

--- includes/functions.php
+++ includes/functions.php
@@ -188,32 +188,6 @@ function buddyforms_contact_author_table_edit_column( $action_html, $post_id, $f
 	return $action_html;
 }
 
-add_action( 'buddyforms_the_loop_after_actions', 'buddyforms_contact_author_the_loop_actions' );
-function buddyforms_contact_author_the_loop_actions( $post_id ) {
-	global $buddyforms;
-
-	$form_slug = get_post_meta( $post_id, '_bf_form_slug', true );
-
-	if ( isset( $buddyforms[ $form_slug ]['contact_author_logged_in_only'] ) && ! is_user_logged_in() ) {
-		return;
-	}
-
-	if ( get_post_status( $post_id ) == 'completed' ) {
-		return;
-	}
-
-	if ( isset( $buddyforms[ $form_slug ]['contact_author'] ) ) {
-
-		echo '<ul class="edit_links">';
-		echo '<li>';
-		buddyforms_contact_author_post( $post_id, $form_slug );
-		echo '</li>';
-		echo '</ul>';
-
-	}
-
-}
-
 add_filter( 'buddyforms_contact_author_message_text', 'buddyforms_contact_author_message_text', 1, 3 );
 
 function buddyforms_contact_author_message_text( $emailBody, $post_id, $form_slug ) {
