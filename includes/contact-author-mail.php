e79411fc72a496405516823f60812728288d3902
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-01-07 17:58:47 -0300

    * Added some filter to change the string for the action button, the modal title, the form elements and the modal action button.

--- includes/contact-author-mail.php
+++ includes/contact-author-mail.php
@@ -6,9 +6,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function buddyforms_contact_author_post( $post_id, $form_slug ) {
 	global $buddyforms;
-	$post = get_post( $post_id );
+	$post                    = get_post( $post_id );
+	$string_contact_author   = __( 'Contact the Author', 'buddyforms-contact-author' );
+	$popup_action_btn        = apply_filters( 'buddyforms_contact_author_action_btn', $string_contact_author, $form_slug, $post_id );
+	$popup_title             = apply_filters( 'buddyforms_contact_author_popup_title', $string_contact_author, $form_slug, $post_id );
+	$popup_btn_action_string = apply_filters( 'buddyforms_contact_author_popup_btn_text', $string_contact_author, $form_slug, $post_id );
+
+	$popup_input_email_label_string   = apply_filters( 'buddyforms_contact_author_popup_element_email', __( 'Your eMail Address', 'buddyforms-contact-author' ), $form_slug, $post_id );
+	$popup_input_subject_label_string = apply_filters( 'buddyforms_contact_author_popup_element_subject', __( 'Subject', 'buddyforms-contact-author' ), $form_slug, $post_id );
+	$popup_input_message_label_string = apply_filters( 'buddyforms_contact_author_popup_element_message', __( 'Add a Message', 'buddyforms-contact-author' ), $form_slug, $post_id );
+
 	?>
-	<?php echo '<a id="buddyforms-contact-author-from-post-id-' . $post_id . '" href="#TB_inline?width=800&height=600&inlineId=buddyforms_contact_author_modal_' . $post_id . '" title="' . __( 'Contact the Author', 'buddyforms-contact-author' ) . '" class="thickbox buddyforms-contact-author-popup"><span aria-label="' . __( 'Contact the Author', 'buddyforms-contact-author' ) . '" title="' . __( 'Contact the Author', 'buddyforms-contact-author' ) . '" class="dashicons dashicons-email"> </span> ' . __( 'Contact the Author', 'buddyforms-contact-author' ) . '</a>'; ?>
+	<?php echo '<a id="buddyforms-contact-author-from-post-id-' . $post_id . '" href="#TB_inline?width=800&height=600&inlineId=buddyforms_contact_author_modal_' . $post_id . '" title="' . $popup_title . '" class="thickbox buddyforms-contact-author-popup"><span aria-label="' . $popup_action_btn . '" title="' . $popup_action_btn . '" class="dashicons dashicons-email"> </span> ' . $popup_action_btn . '</a>'; ?>
 
 	<div id="buddyforms_contact_author_modal_<?php echo $post_id ?>" style="display:none;">
 		<div id="buddyforms_contact_author_wrap">
@@ -24,19 +33,19 @@ function buddyforms_contact_author_post( $post_id, $form_slug ) {
 				"prevent" => array( "bootstrap", "jQuery", "focus", '' ),
 				'method'  => 'post'
 			) );
-			$contact_author_form->addElement( new Element_Email( 'Your eMail Address', 'contact_author_email_from_' . $post_id, array( 'required' => 'required' ) ) );
+			$contact_author_form->addElement( new Element_Email( $popup_input_email_label_string, 'contact_author_email_from_' . $post_id, array( 'required' => 'required' ) ) );
 
 			$contact_author_message_subject = isset( $buddyforms[ $form_slug ]['contact_author_message_subject'] ) ? $buddyforms[ $form_slug ]['contact_author_message_subject'] : '';
 			if ( ! empty( $contact_author_message_subject ) ) {
 				$contact_author_message_subject = buddyforms_contact_author_process_shortcode( $contact_author_message_subject, $post, $form_slug );
 			}
-			$contact_author_form->addElement( new Element_Textbox( 'Subject', 'contact_author_email_subject_' . $post_id, array( 'value' => $contact_author_message_subject ) ) );
+			$contact_author_form->addElement( new Element_Textbox( $popup_input_subject_label_string, 'contact_author_email_subject_' . $post_id, array( 'value' => $contact_author_message_subject ) ) );
 
 			$contact_author_request_message = isset( $buddyforms[ $form_slug ]['contact_author_message_text'] ) ? $buddyforms[ $form_slug ]['contact_author_message_text'] : '';
 			if ( ! empty( $contact_author_request_message ) ) {
 				$contact_author_request_message = buddyforms_contact_author_process_shortcode( $contact_author_request_message, $post, $form_slug );
 			}
-			$contact_author_form->addElement( new Element_Textarea( 'Add a Message', 'contact_author_email_message_' . $post_id, array(
+			$contact_author_form->addElement( new Element_Textarea( $popup_input_message_label_string, 'contact_author_email_message_' . $post_id, array(
 				'value' => $contact_author_request_message,
 				'rows'  => '15',
 				'class' => ''
@@ -45,7 +54,7 @@ function buddyforms_contact_author_post( $post_id, $form_slug ) {
 			$contact_author_form->render();
 			?>
 			<br>
-			<button id="buddyforms_contact_author_<?php echo $post_id ?>" data-post_id="<?php echo $post_id ?>" data-form_slug="<?php echo $form_slug ?>" class="btn-primary btn buddyforms-contact-author-action"><?php echo __( 'Contact the Author', 'buddyforms' ); ?></button>
+			<button id="buddyforms_contact_author_<?php echo $post_id ?>" data-post_id="<?php echo $post_id ?>" data-form_slug="<?php echo $form_slug ?>" class="btn-primary btn buddyforms-contact-author-action"><?php echo $popup_btn_action_string; ?></button>
 		</div>
 	</div>
 	<?php
